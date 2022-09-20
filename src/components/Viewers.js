8b8a4342d7d27edeb7c63d47b897494ce32d6db4
Suriya Prakash <suriyaprakash050301@gmail.com>
2022-09-20 10:48:39 +0530

    ADD:video

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -6,18 +6,33 @@ function Viewers() {
         <div className='viewers'>
             <div className='viewers__wrap'>
                 <img src='images/viewers-disney.png' alt='' />
+                <video autoplay="" loop muted>
+                    <source src='/videos/1564674844-disney.mp4' type='video/mp4' />
+                </video>
             </div>
             <div className='viewers__wrap'>
                 <img src='images/viewers-pixar.png' alt='' />
+                <video autoplay="" loop muted>
+                    <source src='/videos/1564676714-pixar.mp4' type='video/mp4' />
+                </video>
             </div>
             <div className='viewers__wrap'>
                 <img src='images/viewers-marvel.png' alt='' />
+                <video autoplay="" loop muted>
+                    <source src='/videos/1564676115-marvel.mp4' type='video/mp4' />
+                </video>
             </div>
             <div className='viewers__wrap'>
                 <img src='images/viewers-starwars.png' alt='' />
+                <video autoplay="" loop muted>
+                    <source src='/videos/1608229455-star-wars.mp4' type='video/mp4' />
+                </video>
             </div>
             <div className='viewers__wrap'>
                 <img src='images/viewers-national.png' alt='' />
+                <video autoplay="" loop muted>
+                    <source src='/videos/1564676296-national-geographic.mp4' type='video/mp4' />
+                </video>
             </div>
         </div>
     )
